fd8fa315164af2aa655e3772c245e547948cf485
Stefanie Contreras <stefsnow@gmail.com>
2018-03-26 11:43:21 -0700

    Get everything set up

---
+++ src/pages/learn.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page padding>
+    <div> Learn stuff here</div>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "learn"
+};
+</script>
+
+<style>
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    path: "/",
+    component: () => import("layouts/default"),
+    children: [{ path: "", component: () => import("pages/index") }]
+  },
+  {
+    path: "/learn",
+    component: () => import("pages/learn")
+  },
+
+  {
+    // Always leave this as last one
+    path: "*",
+    component: () => import("pages/404")
+  }
+];
